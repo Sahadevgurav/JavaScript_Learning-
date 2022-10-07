9384b7bcaa108f016d6f88fb6fb462f873323a9c
Sahadevgurav <sahadevgurav@gmail.com>
2022-10-07 17:02:41 +0530

    demo.js
    demo.js

---
+++ demo.js
@@ -0,0 +1,12 @@
+// This is my first program
+//  Now I Know the two ways to run program */
+console.log("Hello");
+console.log("My name is sahadev Gurav");
+console.info("This is info");
+console.log("My Hobbies-1.Dancing 2.Travling 3.Reading ");
+console.info("My Streangth- 1.Hardworking 2.Self Motiveted 3. Mange time");
+console.warn("My Weakness-1.Overthinking 2.Being Strate Forword 3.Emotional");
+console.log("30+50")
+console.log("49*479");
+console.warn('I am not that bad guy'); //warn means warning
+console.error("Opps failed to execute");
